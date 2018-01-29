9433ac1b804d7ded5eca148cb06d1ee59f508bd4
slamperone <adrian@develupme.com>
2018-01-29 16:26:15 -0600

    integracion login

---
+++ application/controllers/Logeado.php
@@ -0,0 +1,47 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+class Logeado extends CI_Controller {
+
+
+
+ function index(){
+   $this->load->model('user','',TRUE);
+   $this->load->library('form_validation');
+   $this->form_validation->set_message('required', 'El campo {field} es obligatorio');
+   $this->form_validation->set_message('min_length', '{field} debe contener almenos {param} caracteres');
+   $this->form_validation->set_rules('correo', 'Correo', 'trim|required|min_length[4]|prep_for_form');
+   $this->form_validation->set_rules('pin', 'PIN', 'trim|required|min_length[4]|prep_for_form|callback_check_database');
+
+   if($this->form_validation->run() == FALSE){
+     $this->load->view('login');
+   }else{
+    //Go to private area
+     redirect('dashboard', 'refresh');
+   }
+
+ }
+
+ function check_database($pin){
+   $mail = $this->input->post('correo');
+
+   $result = $this->user->login($mail, $pin);
+
+   if($result){
+     $sess_array = array();
+     foreach($result as $row){
+       $sess_array = array(
+         'id' => $row->idUsuario,
+         'nombre' => $row->nombres,
+         'apellido' => $row->apellidos,
+         'mail' => $row->correo,
+         'genero' => $row->genero
+         );
+       $this->session->set_userdata('logged_in', $sess_array);
+     }
+     return TRUE;
+   }else{
+     $this->form_validation->set_message('check_database', 'Datos inválidos, reintente');
+     return false;
+   }
+ }
+}
+?>
---
+++ application/models/login.php
@@ -0,0 +1,24 @@
+<?php
+Class Login extends CI_Model
+{
+ function login($mail, $pin)
+ {
+   $this -> db -> select('*');
+   $this -> db -> from('usuarios');
+   $this -> db -> where('correo', $mail);
+   $this -> db -> where('pin', MD5($pin));
+   $this -> db -> limit(1);
+
+   $query = $this -> db -> get();
+
+   if($query -> num_rows() == 1)
+   {
+     return $query->result();
+   }
+   else
+   {
+     return false;
+   }
+ }
+}
+?>
